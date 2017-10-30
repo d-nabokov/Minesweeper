e8352c61c2c036314a097e66b52de4819fd252b5
Chocco-Crokko <zastiralkin@gmail.com>
2017-10-30 22:15:27 +0300

    add probabilities

--- src/Probabilities.py
+++ src/Probabilities.py
@@ -1,8 +1,11 @@
+DELTA = 0.01
+
+
 def sum_ind(a, b):
     return 1 - (1 - a) * (1 - b)
 
 
-def get_probs(groups):
+def get_raw_probs(groups):
     probs = {}
     for group in groups:
         for cell in group.cells:
@@ -15,12 +18,40 @@ def get_probs(groups):
     return probs
 
 
+def correct_probs(probs, groups):
+    cycles = 0
+    while True:
+        cycles += 1
+        changed = False
+        for group in groups:
+            s = sum(probs[cell] for cell in group.cells)
+            print('diff: {}, sum = {}, expected {}'.format(abs(s - group.w), s, group.w))
+            if not abs(s - group.w) < DELTA:
+                changed = True
+                k = group.w / s
+                for cell in group.cells:
+                    probs[cell] *= k
+
+        if not changed:
+            break
+
+        print('*************')
+
+    print('algorithm did {} cycles for {} groups'.format(cycles, len(groups)))
+
+
+def get_probs(game):
+    probs = get_raw_probs(game.groups)
+    correct_probs(probs, game.groups)
+    return probs
+
+
 def print_probs(probs, game):
     for i in range(game.m):
         for j in range(game.n):
             if (i, j) in probs:
                 print('{0:6.2f}'.format(probs[(i, j)]), end='')
             else:
-                print('{0:6d}'.format(game.field[i][j]), end='')
+                print('{0:3d}(с)'.format(game.field[i][j]), end='')
 
         print('\n', end='')
--- test/ProbabilitiesTest.py
+++ test/ProbabilitiesTest.py
@@ -7,6 +7,9 @@ class ProbabilitiesTest(unittest.TestCase):
     def test_get_probs(self):
         game = Parser.parse_game('inputs/input1')
 
-        probs = Probabilities.get_probs(game.groups)
+        probs = Probabilities.get_raw_probs(game.groups)
+        Probabilities.print_probs(probs, game)
+
+        Probabilities.correct_probs(probs, game.groups)
         Probabilities.print_probs(probs, game)
 
