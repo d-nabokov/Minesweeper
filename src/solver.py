972e61c427413715fdb0eb5c33d254d040817d1d
Chocco-Crokko <zastiralkin@gmail.com>
2017-11-13 23:50:58 +0300

    Add printing probabilities

--- src/solver.py
+++ src/solver.py
@@ -1,3 +1,4 @@
+import src.probabilities as probabilities
 import src.groups_solver as groups_solver
 import src.parser as parser
 
@@ -6,11 +7,17 @@ def solve(input_file, output_file):
     game = parser.parse_game(input_file)
     with open(output_file, 'wt') as fout:
         mines, not_mines = groups_solver.solve(game.groups)
-        if mines and not_mines:
-            print('1', file=fout)
+        if mines or not_mines:
             for x, y in mines:
                 print(x, y, file=fout)
 
             print('-', file=fout)
             for x, y in not_mines:
                 print(x, y, file=fout)
+        else:
+            probs = probabilities.get_probs(game.groups)
+            # sort first by probability, then by cell (for lexicographical order)
+            sorted_probs = sorted(probs.items(), key=lambda prob: (prob[1], prob[0]))
+            for cell, p in sorted_probs:
+                x, y = cell
+                print("{0} {1}\t{2:.2f}".format(x, y, p), file=fout)
